ace3d95fe49143795abae98809adbd14fb35cafd
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-10-25 11:56:50 -0600

    Added more info to startup script runs.

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -228,7 +228,7 @@ if [ "$originalArgOne" = 'mongod' ]; then
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
 				*.sh) echo "$0: running $f"; . "$f" ;;
-				*.js) echo "$0: running $f"; "${mongo[@]}" "$MONGO_INITDB_DATABASE" "$f"; echo ;;
+				*.js) echo "$0: running ${mongo[@]} $MONGO_INITDB_DATABASE $f"; "${mongo[@]}" "$MONGO_INITDB_DATABASE" "$f"; echo ;;
 				*)    echo "$0: ignoring $f" ;;
 			esac
 			echo
